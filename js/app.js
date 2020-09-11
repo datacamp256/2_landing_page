367ae8713f4ceaacf4d58d729d57aa32634461fe
Data Camp256 <68553874+datacamp256@users.noreply.github.com>
2020-09-11 12:36:18 +0200

    feat: scroll to link target from navigation

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@
 const NAVIGATION_NAME_ATTRIBUTE = 'data-nav';
 const NAVBAR_LIST_IDENTIFIER = 'navbar__list';
 const LIST_ITEM_CLASS = 'menu__link';
+const NAVBAR_LINK = 'menu__link-anchor'
 const ACTIVE_SECTION_CLASS = 'your-active-class';
 const PAGE_HEADER_CLASS = 'page__header';
 let sections;
@@ -76,6 +77,7 @@ function createNavigationMenu() {
         const hyperLink = document.createElement('a');
         hyperLink.textContent = entry.name;
         hyperLink.setAttribute('href', '#' + entry.target);
+        hyperLink.classList.add(NAVBAR_LINK);
         const listElement = document.createElement('li');
         listElement.appendChild(hyperLink)
         listElement.classList.add(LIST_ITEM_CLASS);
@@ -111,6 +113,21 @@ function activateNavigationItem() {
 
 // Scroll to anchor ID using scrollTO event
 
+function findScrollTarget(target) {
+    let foundSection = activeSection;
+    sections.forEach((section) => {
+        if (section.id === (target)) {
+            foundSection = section;
+        }
+    })
+    return foundSection;
+}
+
+function scrollToElement(scrollTarget) {
+    const rect = scrollTarget.querySelector('h2').getBoundingClientRect();
+    const visibleTopEdgeBelow = document.querySelector('.' + PAGE_HEADER_CLASS).offsetHeight
+    window.scrollBy({top: rect.top - visibleTopEdgeBelow, behavior: 'smooth'});
+}
 
 /**
  * End Main Functions
@@ -127,6 +144,14 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // Scroll to section on link click
+document.addEventListener('click', function (event) {
+    let element = event.target;
+    if (element.classList.contains(NAVBAR_LINK)) {
+        event.preventDefault();
+        let scrollTarget = findScrollTarget(element.getAttribute('href').slice(1));
+        scrollToElement(scrollTarget);
+    }
+});
 
 // Set sections as active
 
